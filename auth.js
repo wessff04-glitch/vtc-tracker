200e5cf6154d44665779d7e0207b48af6df87e07
Wassim Hida <wassim@MacBook-Air-de-Wassim.local>
2026-08-12 23:49:37 +0200

    Fix inscription

--- auth.js
+++ auth.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Configuration et initialisation de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCgaSbAqE7f2KskhAvKqU5Zq8rhXQrhrk8",
+  authDomain: "vtc-tracker-318fc.firebaseapp.com",
+  projectId: "vtc-tracker-318fc",
+  storageBucket: "vtc-tracker-318fc.firebasestorage.app",
+  messagingSenderId: "896728030893",
+  appId: "1:896728030893:web:d28d910f74e758fe46b024",
+  measurementId: "G-HPE7XH1MK4"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+
+document.addEventListener('DOMContentLoaded', function() {
     const auth = firebase.auth();
     const db = firebase.firestore();
 
